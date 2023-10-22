7959d9bf122169e60336a364bad4695c48cb4e64
syeda khoula abid <syeda.khoula.abid234@gmail.com>
2023-10-22 17:13:37 +0500

    asking for inputs

--- hacks/retire.c
+++ hacks/retire.c
@@ -3,6 +3,11 @@
 int main()
 {
     double initial_balance, monthly_contribution , annual_rate_of_return , annual_rate_of_inflation;
+    printf("Enter the initial balance, monthly contribution, annual rate of return, annual rate of inflation\n");
+    scanf("%1f,%1f,%1f,%1f", &initial_balance , &monthly_contribution , &annual_rate_of_return , &annual_rate_of_inflation);
 
-    
+    int years_till_retirement;
+    printf("enter the number of years till retirement");
+    scanf("%d",&years_till_retirement);
+
 }
